04359d4a5917683a8ffecd56e9b93ade88fd5086
Benjamin Tondase <benjamin1tondase211@gmail.com>
2021-07-23 17:39:44 +0200

    feat : add header component

---
+++ src/components/About/About.jsx
@@ -0,0 +1,10 @@
+import "./About.scss";
+
+const About = () => {
+    return(
+        <div className="about">
+            <h2>A propos</h2>
+        </div>
+    )
+}
+export default About;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,11 @@
-import Header from "../Header/Header"
+import Header from "../Header/Header";
+import About from "../About/About";
 
 const Home = () => {
     return(
         <div>
             <Header />
+            <About />
         </div>
     )
 }
